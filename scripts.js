1a4efa6775773c11e26bcf39c2ef4f417bd58c18
Hamad-Alfandi <Hamad.alfandi7@gmail.com>
2024-01-20 21:08:53 +0300

    init the empty board with 6x7 with css style

--- scripts.js
+++ scripts.js
@@ -1,2 +1,28 @@
+const ROWS = 6
+const COLS = 7
 
+let currentPlayer = 1
+let board = [
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0],
+]
 
+function renderBoard() {
+  const boardContainer = document.getElementById("connectFourBoard")
+  boardContainer.innerHTML = ""
+
+  for (let row = 0; row < ROWS; row++) {
+    for (let col = 0; col < COLS; col++) {
+      const cell = document.createElement("div")
+      cell.classList.add("cell", "empty")
+      console.log(cell)
+      boardContainer.appendChild(cell);
+    }
+  }
+}
+
+renderBoard()
