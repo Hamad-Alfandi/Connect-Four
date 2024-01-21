e4a31f25dabb9f07680bff2d16d6f0834d338608
Hamad-Alfandi <Hamad.alfandi7@gmail.com>
2024-01-21 09:38:02 +0300

    add function to set at last empty row

--- scripts.js
+++ scripts.js
@@ -19,7 +19,7 @@ function renderBoard() {
     for (let col = 0; col < COLS; col++) {
       const cell = document.createElement("div")
       cell.classList.add("cell", "empty")
-      console.log(cell)
+      // console.log(cell)
       cell.setAttribute("data-row", row)
       cell.setAttribute("data-col", col)
       cell.addEventListener("click", () => {
@@ -34,7 +34,58 @@ function handleCellClick(row, col) {
   const clickedRow = row
   const clickedCol = col
 
-  
+  // console.log(clickedRow)
+  //fn when user click col 1 will add 1 at first empty col
+//   if (!board[clickedRow][clickedCol]) {
+//     board[clickedRow][clickedCol] = currentPlayer
+//     // displayToken(clickedRow, clickedCol)
+//     // checkForWin()
+//     // changeCurrentPlayer()
+//     } else {
+//       alert('This spot is already occupied!')
+//       }
+//       console.log(board)
+
+//if I click  any cell it at first col it will set 1 at the end of first col last empty row
+
+// board[clickedRow][clickedCol] = currentPlayer
+
+  for(let i=5 ; i>=0 ; i--){
+  //   //set 1 at last empty row
+    if(board[i][clickedCol] ===0){
+      board[i][clickedCol]=currentPlayer
+      break
+      }
+
+  }
+
+
+console.log(board)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 renderBoard()
